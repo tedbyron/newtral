183df5f2cc4f916013588710198b038a264c70ea
Theodore Byron <ted@tedbyron.com>
2020-02-25 13:47:54 -0500

    Remove emoji dude

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,10 +10,7 @@ const NotFoundPage = () => (
     <section className="section">
       <div className="container has-text-centered">
         <h1 className="title is-1">404</h1>
-        <h2 className="subtitle">
-          {'That route doesn\'t exist... '}
-          <span role="img" aria-label="frowning face with open mouth emoji">😦</span>
-        </h2>
+        <h2 className="subtitle">That route doesn&apos;t exist...</h2>
       </div>
     </section>
   </Layout>
